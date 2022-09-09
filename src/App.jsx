0d263053acf41d88c741f7d93b71db88f1f5783b
Rafael Olivay <rafaelsebastian@live.com.ar>
2022-09-09 18:26:31 -0300

    add environments variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App() {  
+
+  const API_key = import.meta.env.VITE_RAPID_API_KEY
+  const [data, setData] = useState()
+  const lat = 39.31
+  const lon = -74.5
+
+ useEffect(() => {
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': API_key,
+      'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+  };
+  
+  fetch('https://wft-geo-db.p.rapidapi.com/v1/geo/cities', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+ }, [])
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      olis  
     </div>
   )
 }
