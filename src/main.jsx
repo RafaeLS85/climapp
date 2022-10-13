b55dfa75dc4ab2f5e50b38d6c483fe9ac0372dfc
Rafael S. Olivay <47649965+RafaeLS85@users.noreply.github.com>
2022-10-13 01:41:24 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-// React.StrictMode renders components twice
-// https://stackoverflow.com/questions/60618844/react-hooks-useeffect-is-called-twice-even-if-an-empty-array-is-used-as-an-ar
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
     <App />
